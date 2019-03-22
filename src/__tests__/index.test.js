cbd618219dcab8720793c915f692dfc1a7229e09
Luc Patiny <luc@patiny.com>
2019-03-22 11:13:59 +0100

    add test load / save json

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,9 +3,9 @@ import { join } from 'path';
 
 import { fromText } from '..';
 
-import { exportAllDeclaration } from '@babel/types';
+import { IRSpectrum } from '../IRSpectrum';
 
-test('Full test', () => {
+test('Test load / save json', () => {
   let text = readFileSync(
     join(__dirname, '../../testFiles/simple.txt'),
     'utf8'
@@ -13,4 +13,7 @@ test('Full test', () => {
   let irSpectrum = fromText(text);
   let json = irSpectrum.toJSON();
   expect(json).toStrictEqual({ kind: 2, wavelength: [1, 2, 3], y: [2, 3, 2] });
+  let irSpectrum2 = new IRSpectrum(json);
+  let json2 = irSpectrum2.toJSON();
+  expect(json).toStrictEqual(json2);
 });
