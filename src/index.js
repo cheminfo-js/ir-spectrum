c52051527cc0fd5165427935693c30b5a2cca059
Luc Patiny <luc@patiny.com>
2019-04-04 11:12:14 +0200

    export Spectra

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { Spectrum } from './Spectrum';
+export { Spectra } from './Spectra';
 export { fromJcamp } from './from/jcamp';
 export { fromText } from './from/text';
 export { ABSORBANCE, TRANSMITTANCE, PERCENT_TRANSMITTANCE } from './constants';
